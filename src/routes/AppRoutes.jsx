820054823f2246bf858fbc7affc40b853c3500c3
TNG <kennsantos211@gmail.com>
2026-09-05 15:47:47 +0800

    update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,21 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
-import AdminDashboard from "../pages/admin/AdminDashboard";
+import Dashboard from "../pages/Dashboard";
 import StudentDashboard from "../pages/StudentDashboard";
 import RoleRoute from "../services/RoleRoute";
 import AdminLayout from "../layouts/AdminLayout";
-import Application from "../pages/admin/Application";
-import Students from "../pages/admin/Students";
+import Candidates from "../pages/Candidates";
 import Positions from "../pages/Positions";
-import Attendance from "../pages/admin/Attendance";
-import OjtHours from "../pages/admin/OjtHours";
-import Evaluation from "../pages/admin/Evaluation";
-import OjtReports from "../pages/admin/OjtReports";
-import AttendanceReports from "../pages/admin/AttendanceReport";
-import Users from "../pages/admin/Users";
-import Settings from "../pages/admin/Setting";
-import OjtCoordinators from "../pages/admin/OjtCoordinators";
-import CoordinatorsDashboard from "../pages/admin/Coordinator/CoordinatorsDashboard";
 
 function AppRoutes() {
   return (
@@ -30,18 +20,9 @@ function AppRoutes() {
             </RoleRoute>
           }
         >
-            <Route index element={<AdminDashboard />} />
-            <Route path="students" element={<Students />} />
-            <Route path="application" element={<Application />} />
+            <Route index element={<Dashboard />} />
+            <Route path="students" element={<Candidates />} />
             <Route path="partnercompanies" element={<Positions />} />
-            <Route path="attendance" element={<Attendance />} />
-            <Route path="ojthours" element={<OjtHours />} />
-            <Route path="evaluation" element={<Evaluation />} />
-            <Route path="ojtreports" element={<OjtReports />} />
-            <Route path="attendancereports" element={<AttendanceReports />} />
-            <Route path="users" element={<Users />} />
-            <Route path="ojtcoordinators" element={<OjtCoordinators />} />
-            <Route path="settings" element={<Settings />} />
         </Route>
 
         <Route
@@ -49,14 +30,6 @@ function AppRoutes() {
           element={
             <RoleRoute allowedRole="student">
               <StudentDashboard />
-            </RoleRoute>
-          }
-        />
-        <Route
-          path="/coordinator"
-          element={
-            <RoleRoute allowedRole="coordinator">
-              <CoordinatorsDashboard />
             </RoleRoute>
           }
         />
