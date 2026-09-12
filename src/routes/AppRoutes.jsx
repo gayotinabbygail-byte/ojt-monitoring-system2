bbfbb3d8bec015f7aa1926bdda95e2b43129684e
TNG <kennsantos211@gmail.com>
2026-09-12 11:26:57 +0800

    UPDATE
    COMMIT TO MAIN

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -12,6 +12,7 @@ import Students from "../pages/Students";
 import PartnerCompanies from "../pages/PartnerCompanies";
 import OJTCoordinators from "../pages/OJTCoordinators";
 import OJTHours from "../pages/OJTHours";
+import Evaluation from "../pages/Evaluation";
 
 const placeholderRoutes = [
   ["ojtcoordinators", "OJT Coordinators"],
@@ -60,7 +61,8 @@ function AppRoutes() {
           <Route path="partnercompanies" element={<PartnerCompanies />} />
           <Route path="ojtcoordinators" element={<OJTCoordinators />} />
           <Route path="ojthours" element={<OJTHours />} />
-          {placeholderRoutes.filter(([path]) => !["ojtcoordinators", "ojthours"].includes(path)).map(([path, title]) => (
+          <Route path="evaluation" element={<Evaluation />} />
+          {placeholderRoutes.filter(([path]) => !["ojtcoordinators", "ojthours", "evaluation"].includes(path)).map(([path, title]) => (
             <Route key={path} path={path} element={<AdminPlaceholder title={title} />} />
           ))}
         </Route>
